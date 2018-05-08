fea8417c5f75f6bad7ffc8980472525fb543906e
Brandon Murry <murryb@amazon.com>
2018-05-08 11:03:28 -0700

    #14 display a warning if you exceed 100 Kudos in one session

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,33 +1,33 @@
 let kudosBtns = []
   , KUDOS_INTERVAL = 1000 // in ms
+  , KUDOS_LOCKOUT = 100 // https://github.com/o2dazone/StravaKudos/issues/13#issuecomment-356319221
   , btn
-  , d = document;
+  , d = document
+  , els = '.activity button.js-add-kudo, .group-activity button.js-add-kudo';
 
 function init() {
-  // create button
-
-  styles = d.createElement('style');
-  styles.innerHTML = '#stravaKudos{left:5px;font-size:20px;box-shadow:0 2px 1px rgba(0,0,0,0.2);z-index:49;position:fixed;top:61px}#stravaKudos.hidden{display:none !important;visibility:hidden !important}#stravaKudosCount{font-weight:bold}';
-  d.head.appendChild(styles);
+  const styles = d.createElement('style');
+  styles.innerHTML = '#stravaKudos{display:flex;flex-direction:column;left:5px;font-size:20px;box-shadow:0 2px 1px rgba(0,0,0,0.2);z-index:49;position:fixed;top:61px}#stravaKudos div{margin:0 auto}#stravaKudos p{margin:0;font-size:14px}#stravaKudos.hidden,#stravaKudos.lockout p{display:none !important;visibility:hidden !important}#stravaKudosCount{margin:0 3px;font-weight:bold}';
+  d.head.prepend(styles);
 
   btn = d.createElement('button');
   btn.id = 'stravaKudos';
-  btn.innerHTML = 'Give <span id="stravaKudosCount"></span> Auto-Kudos';
+  btn.innerHTML = `
+    <div>Give <span id="skCount"></span> Kudos</div>
+    <p>Strava may throttle too<br/>many Kudos in one session</p>
+  `;
   btn.className = 'btn btn-sm btn-primary hidden';
 
-  btn.onclick = function(e) {
-    e.preventDefault();
-    giveKudos();
-  }
-
-  d.body.appendChild(btn);
+  btn.addEventListener('click', giveKudos);
+  d.body.prepend(btn);
   updateCountNum();
 }
 
 // give all the kudos
 function giveKudos() {
+  btn.removeEventListener('click', giveKudos);
   setTimeout(() => {
-    const kudoBtn = d.querySelector('.activity button.js-add-kudo, .group-activity button.js-add-kudo');
+    const kudoBtn = d.querySelector(els);
     if(kudoBtn) {
       kudoBtn.click();
       giveKudos();
@@ -35,9 +35,17 @@ function giveKudos() {
   }, KUDOS_INTERVAL);
 }
 
+// toggle box styles
 function toggleKudosBox() {
-  if (kudosBtns.length) {
+  const num = kudosBtns.length;
+  if (num) {
     btn.classList.remove('hidden');
+
+    if (num < KUDOS_LOCKOUT) {
+      btn.classList.add('lockout');
+    } else {
+      btn.classList.remove('lockout');
+    }
   } else {
     btn.classList.add('hidden');
   }
@@ -45,11 +53,11 @@ function toggleKudosBox() {
 
 // publish number of kudos
 function updateCountNum() {
-  const count = d.getElementById('stravaKudosCount');
+  const count = d.getElementById('skCount');
 
   if (count) {
     setInterval(() => {
-      kudosBtns = d.querySelectorAll('.activity button.js-add-kudo, .group-activity button.js-add-kudo');
+      kudosBtns = d.querySelectorAll(els);
       count.innerHTML = kudosBtns.length;
       toggleKudosBox();
     }, KUDOS_INTERVAL);
